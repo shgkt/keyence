bfacc91e6c1fc1704f4f2e2348c73119b1e25c8e
ShogoKato <kato3812@gmail.com>
2021-03-25 09:08:44 +0900

    update 平行線の垂直線を引けるように

--- ParallelLineCanvas.py
+++ ParallelLineCanvas.py
@@ -16,7 +16,7 @@ class ParallelLineCanvas:
         self.line = []
         # 直線の式保持用 ax + by + c
         self.a = 0
-        self.b = 0
+        self.b = -1
         self.c = 0
         # 直線を伸ばすための値
         self.times = 1000
@@ -37,26 +37,34 @@ class ParallelLineCanvas:
             self.start_x = event.x
             self.start_y = event.y
             self.motion_bind_id = self.canvas.bind(
-                '<Motion>', self.button_move)
+                '<Motion>', self.mouse_move)
             self.drawing_line = self.draw_first_line(event)
         else:
             self.line.append(self.draw_first_line(event))
             self.second_line = True
+            self.calc_lenear_func()
+            self.drawing_vertical_line = self.draw_vertical_line(event)
+
         return self.start_x, self.start_y
 
-    def button_move(self, event):
+    def mouse_move(self, event):
+        # 1本目
         if not self.second_line:
             self.canvas.delete(self.drawing_line)
             # 傾きを出して視点と終端を決める
-            self.slope = (event.y - self.start_y) / \
-                (event.x - self.start_x)
+            self.slope = self.calc_slope(event)
             self.drawing_line = self.draw_first_line(event)
+        # 2本目
         else:
             self.canvas.delete(self.drawing_line)
             self.drawing_line = self.draw_second_line(event)
+            self.canvas.delete(self.drawing_vertical_line)
+            self.drawing_vertical_line = self.draw_vertical_line(event)
 
-    def get_distance(self, x1, y1, x2, y2):
-        d = round(math.sqrt((x2-x1)**2 + (y2-y1)**2), 1)
+    def get_distance(self, event):
+        numerator = abs(self.a * event.x + self.b * event.y + self.c)
+        denominator = math.sqrt(self.a**2 + self.b**2)
+        d = numerator / denominator
         return d
 
     def draw_first_line(self, event):
@@ -67,6 +75,26 @@ class ParallelLineCanvas:
         return self.draw_line(event.x - self.times,
                               event.y - self.slope * self.times, event.x + self.times, event.y + self.slope * self.times, "black")
 
+    def draw_vertical_line(self, event):
+        d = self.get_distance(event)
+        vs = - 1 / self.slope  # vertical slope
+        dx = d / math.sqrt(vs**2 + 1)
+        dy = d * vs / math.sqrt(vs**2 + 1)
+        print(dx)
+        print(dy)
+        return self.draw_line(event.x,
+                              event.y, event.x - dx, event.y - dy, "black")
+
+    def calc_slope(self, event):
+        if event.x - self.start_x == 0:
+            return 0
+
+        return (event.y - self.start_y) / (event.x - self.start_x)
+
     def draw_line(self, x1, y1, x2, y2, color):
         return self.canvas.create_line(
             x1, y1, x2, y2, fill=color)
+
+    def calc_lenear_func(self):
+        self.c = - self.slope * self.start_x + self.start_y
+        self.a = self.slope
